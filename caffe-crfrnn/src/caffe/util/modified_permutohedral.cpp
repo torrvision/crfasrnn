f853d1065b22fcb3de66bc54af7f8cde8187ae54
sebastien <sebastien.ehrhardt@gmail.com>
2015-11-01 21:43:32 +0100

    modified_permutohedral.cpp : change order to avoid compile warnings.

--- caffe-crfrnn/src/caffe/util/modified_permutohedral.cpp
+++ caffe-crfrnn/src/caffe/util/modified_permutohedral.cpp
@@ -102,7 +102,7 @@ public:
 /***          ModifiedPermutohedral Lattice           ***/
 /************************************************/
 
-ModifiedPermutohedral::ModifiedPermutohedral():N_( 0 ), M_( 0 ), d_( 0 ), is_init(false) {
+ModifiedPermutohedral::ModifiedPermutohedral(): is_init(false), N_( 0 ), M_( 0 ), d_( 0 ) {
 }
 #ifdef SSE_PERMUTOHEDRAL
 void ModifiedPermutohedral::init_cpu(const float* features, int num_dimensions, int num_points)
